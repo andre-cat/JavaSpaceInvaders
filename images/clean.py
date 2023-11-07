4e5e0b7905867b35ccbffbff92b5e73b499e7833
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-11-06 21:07:52 -0500

    Adding some functions

--- images/clean.py
+++
@@ -1,8 +0,0 @@
-
-import os
-
-folder_path = "/home/aiker/Workspace/Java/JavaSpaceInvaders/images"
-
-for filename in os.listdir(folder_path):
-    if filename.endswith("_1.wav") or filename.endswith("_1.gif") or filename.endswith("_1.png"):
-        os.remove(os.path.join(folder_path, filename))
