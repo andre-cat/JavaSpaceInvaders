b337302c48bfe00f4656f5c50c9df8e7c89ebc57
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-11-06 21:07:27 -0500

    Deleting unnecessary files

--- src/space/invaders/Pantalla.java
+++ src/space/invaders/Pantalla.java
@@ -20,6 +20,39 @@ public final class Pantalla extends JFrame {
     // named "alienPositions" with 10 rows and 10 columns
     public static boolean[][] alienPositions = new boolean[10][10];
 
+    // Create a function that receives a matrix of 10x10 booleans
+    // and changes the value of the first five rows to true except by the first
+    // and the last column
+    public static void setAlienPositions(boolean[][] alienPositions) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 1; j < 9; j++) {
+                alienPositions[i][j] = true;
+            }
+        }
+    }
+
+    // Create a function that receives a row, a column, a height and a width
+    // and returns the position of the alien in the screen
+    public static int[] getAlienPosition(int row, int column, int height, int width) {
+        int[] position = new int[2];
+        position[0] = (width / 10) * column;
+        position[1] = (height / 10) * row;
+        return position;
+    }
+
+    // Create a function named colorAlien that receives a row, a column
+    // and returns a path to the image of the alien
+    public static String colorAlien(int row, int column) {
+        if (row == 0 || row == 1) {
+            return "src/space/invaders/images/alien1.png";
+        } else if (row == 2 || row == 3) {
+            return "src/space/invaders/images/alien2.png";
+        } else {
+            return "src/space/invaders/images/alien3.png";
+        }
+    }
+    
+
     // When press the run show the window
     public Pantalla() {
         initUI();
